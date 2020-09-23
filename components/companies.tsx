5423787a3251c00e95bb62378302430325855df1
Arish Shah <arish0246@gmail.com>
2020-09-23 23:13:36 +0530

    companies center

--- components/companies.tsx
+++ components/companies.tsx
@@ -1,41 +1,36 @@
 import Layout from './layout';
 
 function Companies() {
+  const images: { src: string; alt: string; className: string }[] = [
+    {
+      src: '/companies/capgemini.svg',
+      alt: 'Capgemini',
+      className: 'h-8',
+    },
+    { src: '/companies/oracle.svg', alt: 'Oracle', className: 'h-5 mx-auto' },
+    {
+      src: '/companies/dell.svg',
+      alt: 'DELL',
+      className: 'h-6 md:h-8 mx-auto',
+    },
+    {
+      src: '/companies/deloitte.svg',
+      alt: 'Deloitte',
+      className: 'h-8 mx-auto',
+    },
+    { src: '/companies/nokia.svg', alt: 'Nokia', className: 'h-6 mx-auto' },
+  ];
+
   return (
     <Layout
       parent="bg-gray-100 mt-2"
       child="py-10 overflow-x-hidden grid grid-cols-3 gap-10 items-center text-center lg:px-6 md:px-32 md:grid-cols-5"
     >
-      <img
-        src="/companies/capgemini.svg"
-        alt="Capgemini"
-        className="h-8"
-        draggable={false}
-      />
-      <img
-        src="/companies/oracle.svg"
-        alt="Oracle"
-        className="h-5"
-        draggable={false}
-      />
-      <img
-        src="/companies/dell.svg"
-        alt="DELL"
-        className="h-6 md:h-8"
-        draggable={false}
-      />
-      <img
-        src="/companies/deloitte.svg"
-        alt="Deloitte"
-        className="h-8"
-        draggable={false}
-      />
-      <img
-        src="/companies/nokia.svg"
-        alt="Nokia"
-        className="h-6"
-        draggable={false}
-      />
+      {images.map(image => (
+        <div key={image.src}>
+          <img {...image} draggable={false} />
+        </div>
+      ))}
     </Layout>
   );
 }
