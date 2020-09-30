95f8d8997915e2288116f90f71d8ba7f3508e1f8
Arish Shah <arish0246@gmail.com>
2020-09-30 18:27:33 +0530

    add carousel to companies

--- components/companies.tsx
+++ components/companies.tsx
@@ -1,4 +1,5 @@
 import Layout from './layout';
+import Slider from 'react-slick';
 
 function Companies() {
   const images: { src: string; alt: string; className: string }[] = [
@@ -21,18 +22,38 @@ function Companies() {
     { src: '/companies/nokia.svg', alt: 'Nokia', className: 'h-6 mx-auto' },
   ];
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    className: 'companies-slick',
+  };
+
   return (
-    <Layout
-      parent="bg-gray-100 mt-2"
-      child="py-10 overflow-x-hidden grid grid-cols-3 gap-10 items-center text-center lg:px-6 md:px-32 md:grid-cols-5"
-      animate={true}
-    >
-      {images.map(image => (
-        <div key={image.src}>
-          <img {...image} draggable={false} />
-        </div>
-      ))}
-    </Layout>
+    <>
+      <Layout
+        parent="bg-gray-100 mt-2 hidden md:block"
+        child="py-10 overflow-x-hidden grid grid-cols-3 gap-10 items-center text-center lg:px-6 md:px-32 md:grid-cols-5"
+        animate={true}
+      >
+        {images.map((image) => (
+          <div key={image.src}>
+            <img {...image} draggable={false} />
+          </div>
+        ))}
+      </Layout>
+      <Layout parent="bg-gray-100 mt-2 md:hidden" child="px-0 py-8">
+        <Slider {...settings}>
+          {images.map((image) => (
+            <img {...image} draggable={false} key={image.src} />
+          ))}
+        </Slider>
+      </Layout>
+    </>
   );
 }
 
