d6649c857755c484108446b6ae343c1d8acf8085
Arish Shah <arish0246@gmail.com>
2020-10-04 16:25:58 +0530

    add mail logic

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'API Hit 💯' });
+async function main() {
+  let testAccount = nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: 'yashjha0007@gmail.com', // list of receivers
+    subject: 'Test Message from nodemailer', // Subject line
+    text: 'How we doing?', // plain text body
+    html: '<b>Hello world?</b>', // html body
+  });
+
+  return info;
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const info = await main();
+  await res.status(200).json({ message: 'Message sent: ' + info.messageId });
 };
